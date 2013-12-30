c9d4136d2bc79896e441006d3224e6f6d5bfbc46
Lyubomir Ganev <luboganev@gmail.com>
2013-12-31 00:05:46 +0100

    The green theme has come

--- GoodIntentionsApp/.apt_generated/com/luboganev/goodintentions/ui/MainActivity$$ViewInjector.java
+++ GoodIntentionsApp/.apt_generated/com/luboganev/goodintentions/ui/MainActivity$$ViewInjector.java
@@ -7,36 +7,26 @@ import butterknife.ButterKnife.Finder;
 public class MainActivity$$ViewInjector {
   public static void inject(Finder finder, final com.luboganev.goodintentions.ui.MainActivity target, Object source) {
     View view;
-    view = finder.findById(source, 2131361797);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131361797' for field 'mData' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.mData = (android.widget.EditText) view;
-    view = finder.findById(source, 2131361798);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131361798' for field 'mMimeType' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.mMimeType = (android.widget.EditText) view;
-    view = finder.findById(source, 2131361793);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131361793' for field 'mContextType' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.mContextType = (android.widget.Spinner) view;
-    view = finder.findById(source, 2131361796);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131361796' for field 'mClassName' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.mClassName = (android.widget.EditText) view;
     view = finder.findById(source, 2131361792);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2131361792' for field 'mType' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.mType = (android.widget.Spinner) view;
-    view = finder.findById(source, 2131361800);
+    view = finder.findById(source, 2131361798);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2131361800' for field 'mIntentionFlags' was not found. If this view is optional add '@Optional' annotation.");
+      throw new IllegalStateException("Required view with id '2131361798' for field 'mMimeType' was not found. If this view is optional add '@Optional' annotation.");
     }
-    target.mIntentionFlags = (com.luboganev.goodintentions.ui.views.IntentionFlagsLinearLayout) view;
+    target.mMimeType = (android.widget.EditText) view;
+    view = finder.findById(source, 2131361794);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131361794' for field 'mAction' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.mAction = (android.widget.EditText) view;
+    view = finder.findById(source, 2131361797);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131361797' for field 'mData' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.mData = (android.widget.EditText) view;
     view = finder.findById(source, 2131361795);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2131361795' for field 'mPackageName' was not found. If this view is optional add '@Optional' annotation.");
@@ -47,22 +37,32 @@ public class MainActivity$$ViewInjector {
       throw new IllegalStateException("Required view with id '2131361799' for field 'mIntentionCategories' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.mIntentionCategories = (com.luboganev.goodintentions.ui.views.IntentionCategoriesLinearLayout) view;
-    view = finder.findById(source, 2131361794);
+    view = finder.findById(source, 2131361796);
     if (view == null) {
-      throw new IllegalStateException("Required view with id '2131361794' for field 'mAction' was not found. If this view is optional add '@Optional' annotation.");
+      throw new IllegalStateException("Required view with id '2131361796' for field 'mClassName' was not found. If this view is optional add '@Optional' annotation.");
     }
-    target.mAction = (android.widget.EditText) view;
+    target.mClassName = (android.widget.EditText) view;
+    view = finder.findById(source, 2131361800);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131361800' for field 'mIntentionFlags' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.mIntentionFlags = (com.luboganev.goodintentions.ui.views.IntentionFlagsLinearLayout) view;
+    view = finder.findById(source, 2131361793);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131361793' for field 'mContextType' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.mContextType = (android.widget.Spinner) view;
   }
 
   public static void reset(com.luboganev.goodintentions.ui.MainActivity target) {
-    target.mData = null;
-    target.mMimeType = null;
-    target.mContextType = null;
-    target.mClassName = null;
     target.mType = null;
-    target.mIntentionFlags = null;
+    target.mMimeType = null;
+    target.mAction = null;
+    target.mData = null;
     target.mPackageName = null;
     target.mIntentionCategories = null;
-    target.mAction = null;
+    target.mClassName = null;
+    target.mIntentionFlags = null;
+    target.mContextType = null;
   }
 }
--- GoodIntentionsApp/.apt_generated/com/luboganev/goodintentions/ui/views/IntentionCategoriesLinearLayout$$ViewInjector.java
+++ GoodIntentionsApp/.apt_generated/com/luboganev/goodintentions/ui/views/IntentionCategoriesLinearLayout$$ViewInjector.java
@@ -7,21 +7,21 @@ import butterknife.ButterKnife.Finder;
 public class IntentionCategoriesLinearLayout$$ViewInjector {
   public static void inject(Finder finder, final com.luboganev.goodintentions.ui.views.IntentionCategoriesLinearLayout target, Object source) {
     View view;
-    view = finder.findById(source, 2131361804);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131361804' for field 'mCategoriesListLinearLayout' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.mCategoriesListLinearLayout = (android.widget.LinearLayout) view;
-    view = finder.findById(source, 2131361802);
-    if (view == null) {
-      throw new IllegalStateException("Required view with id '2131361802' for field 'mNewCategoryEditText' was not found. If this view is optional add '@Optional' annotation.");
-    }
-    target.mNewCategoryEditText = (android.widget.EditText) view;
     view = finder.findById(source, 2131361803);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2131361803' for field 'mCategoryAddButton' was not found. If this view is optional add '@Optional' annotation.");
     }
     target.mCategoryAddButton = (android.widget.ImageButton) view;
+    view = finder.findById(source, 2131361802);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131361802' for field 'mNewCategoryEditText' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.mNewCategoryEditText = (android.widget.EditText) view;
+    view = finder.findById(source, 2131361804);
+    if (view == null) {
+      throw new IllegalStateException("Required view with id '2131361804' for field 'mCategoriesListLinearLayout' was not found. If this view is optional add '@Optional' annotation.");
+    }
+    target.mCategoriesListLinearLayout = (android.widget.LinearLayout) view;
     view = finder.findById(source, 2131361801);
     if (view == null) {
       throw new IllegalStateException("Required view with id '2131361801' for field 'mCategorySearchButton' was not found. If this view is optional add '@Optional' annotation.");
@@ -30,9 +30,9 @@ public class IntentionCategoriesLinearLayout$$ViewInjector {
   }
 
   public static void reset(com.luboganev.goodintentions.ui.views.IntentionCategoriesLinearLayout target) {
-    target.mCategoriesListLinearLayout = null;
-    target.mNewCategoryEditText = null;
     target.mCategoryAddButton = null;
+    target.mNewCategoryEditText = null;
+    target.mCategoriesListLinearLayout = null;
     target.mCategorySearchButton = null;
   }
 }
